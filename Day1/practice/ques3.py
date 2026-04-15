76f4a47a9bf0e9345837a89b100c883a4839ad5e
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-16 00:11:28 +0530

    solve basic problem and use module to perform a task

---
+++ Day1/practice/ques3.py
@@ -0,0 +1,13 @@
+import pyttsx3
+engine = pyttsx3.init()
+
+engine.say("Hello sumit, i'm your personal assistance how's going," \
+"what is your today tasks" \
+"Let me know" \
+"Sumit Reply: Let me coooooook")
+engine.runAndWait()
+
+print("Complete!")
+
+
+# install an external module and use it to perform an operation of your intrest.
