ac0b1a94ff32b0876e48ee4be5d48b0983f76458
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-15 23:40:27 +0530

    revision theory such as what is module, pip etc.

--- Day1/first.py
+++ Day1/first.py
@@ -9,3 +9,13 @@ print("NAME:- "+ A)
 print("EMAIL:- "+ B)
 print("NUMBER:- "+ C)
 print("PASSWORD:- "+ D)
+
+
+""" 
+What is module -> a module is a file containing code written by somebody else which can be imported and used in our programs.
+
+What is pip -> pip is the package manager for python. you can use pip to install a module on your system. (pip install django).
+
+Some important points REPL-> read evaluate print loop -> open terminal and write python then use calculator.
+
+"""
