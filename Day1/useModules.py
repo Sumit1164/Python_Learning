68b0f2e730f52494f4ac96da50d1cab8fcb29c44
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-15 23:41:18 +0530

    use an module is it pyjokes to read jokes and enjoy coding

---
+++ Day1/useModules.py
@@ -0,0 +1,5 @@
+import pyjokes
+
+joke = pyjokes.get_joke() 
+print(joke) 
+# Girl: Do you drink? Programmer: No. Girl: Have Girlfriend? Programmer: No. Girl: Then how do you enjoy life? Programmer: I am Programmer
