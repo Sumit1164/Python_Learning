766a78a88e3aedb618288b3e1d849734131a84a9
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-18 10:50:57 +0530

    variable and datatypes

---
+++ Day2/second.py
@@ -0,0 +1,19 @@
+# variable and datatypes
+
+
+# variable  is a conatiner, is it store values different types
+
+a = 10
+b =7.5
+_c ="Sumit"
+print(b + a)
+
+
+# datatypes
+# is it defines working of each data types means integer can only store int values 
+
+i =1 #integer number
+j = 5.7 # float number
+k = "Sush" # String 
+l = True / False # Boolean 
+m = None # nothing store in m 
