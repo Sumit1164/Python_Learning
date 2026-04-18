4e39dc2b23781b65da5de7e7e4a82cd36fabcf30
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-18 10:51:58 +0530

    Input Function

---
+++ Day2/inputFun.py
@@ -0,0 +1,10 @@
+# Input Function
+a = int(input("Enter number 1: "))
+b = int(input("Enter number 2: "))
+c = int(input("Enter number 3: "))
+
+print("Number of a is : ", a)
+print("Number of b is : ", b)
+print("Number of c is : ", c)
+
+print("Sum of a + b + c = ", a+b+c)
