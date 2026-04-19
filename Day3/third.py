f699fca548205c8d7683d8348d1468e367746f09
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-19 12:25:51 +0530

    ESCAPE SEQUENCE CHARACTERS

--- Day3/third.py
+++ Day3/third.py
@@ -37,6 +37,10 @@ x = word[0:35:4]
 # print(word.lower()) # it make all the character in the word into small latter
 # print(word.upper()) # it make all the character in the word into capital latter
 # print(word.title()) # it make captilizes the first character of each word in the string
+# print(word.find("word"))
+# print(word.replace("is word", " "))
+
+
 
 # Triming and Padding 
 z = "        hello       "
@@ -45,3 +49,17 @@ z = "        hello       "
 # print(z.rstrip()) # removing trailing whitespace
 zx="57"
 # print(zx.zfill(15)) #zfill(width) -> Pads the string with zero on the left, to fill the specified width
+
+
+
+'''ESCAPE SEQUENCE CHARACTERS'''
+
+# print('Hey Everyone \'SUMIT TRIPATHI\' Is Very Unique Man In This World!') # \' hello \' for singale quote print
+# print("Hello\nWorld") # \n for new line
+# print("Hello\tWorld") # \t for space tab
+# print("Hello\vWorld") # Vertical Tab
+# print("Hello\a") # Bell / Alert
+# print("Hello\0World") # Null Character (Null character is invisible; it doesn’t print anything noticeable.)
+# print("Hello\rWorld") #Carriage Return(\r moves the cursor to the beginning, so "World" overwrites "Hello".)
+# print("What Going\\ON!") # \\ for backslash print
+# print("Hey Everyone \"SUMIT TRIPATHI\" Is Very Unique Man In This World!") # \" hello \" for double quote print 
