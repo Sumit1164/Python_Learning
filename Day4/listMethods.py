850f05b0e2cbd49641ebde623436cc624cff10dc
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-20 20:51:00 +0530

    Learning List various Methods  in PYTHON

---
+++ Day4/listMethods.py
@@ -0,0 +1,31 @@
+num =[60, 34, 1, 54, 78, 134, 78, 78, 54, 345, 795, 157]
+
+# List Methods
+
+
+# n = num.append(555) #adding an element at the end of list.
+
+# nx = num.sort() #sort function sort elements in accending order.
+
+# num.reverse() #reverse the list element.
+
+# num.insert(5, 125) #insert the number in the list any possible index.
+
+# num.pop(8) #delete a element from the list, existing index.
+
+# num.pop(-2) #delete a element from the list, existing index(delete at the 2 number element).
+
+# num.extend([4,5,6,278,85,676]) #it use to add more elements in the list at the end.
+
+# num.clear() #Removes all elements from the list.
+
+# x = num.index(78) #Returns index of first occurrence.
+# print(x)
+
+# z = num.count(54) #Counts occurrences of a value 54x2 Times.
+# print(z)
+
+# kx = num.remove(157) #remove the element from the list
+# print(kx)
+
+print(num)
