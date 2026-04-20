a361aa900bb518bc0df5aba84c9e141faae368b1
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-20 20:50:26 +0530

    List  in python

---
+++ Day4/fourth.py
@@ -0,0 +1,18 @@
+# List 
+
+'''
+List are mutable.
+indexing like string.
+store any type of data.
+
+Mutable → You can change elements after creation
+Ordered → Items maintain their position
+Heterogeneous → Can store mixed data types
+'''
+
+group =['Apple', 'Banana', 'Orange', 'Sumit', 5, 5.57, False]
+
+n =len(group)
+group[n-1] =True
+print(group[n-1])
+# print(group)
