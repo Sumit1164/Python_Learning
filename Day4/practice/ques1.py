04d1bf9509919bc888e8e9a8ba6f06ebe90ada9a
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-21 00:42:22 +0530

    practice questions

---
+++ Day4/practice/ques1.py
@@ -0,0 +1,38 @@
+a = [input("Enter seven fruits name: ")]
+print(a)
+
+'''
+Apple
+Banana
+Mango
+Orange
+Pineapple
+Grapes
+Watermelon
+Papaya
+Guava
+Strawberry
+Blueberry
+Kiwi
+Pomegranate
+Peach
+Plum
+Cherry
+Coconut
+Apricot
+Fig
+Lychee
+Dragon fruit
+Passion fruit
+Pear
+Mulberry
+Blackberry
+Cranberry
+Jackfruit
+'''
+
+fruits = []
+f = input("Enter fruit name: ")
+fruits.append(f) 
+
+print(fruits)
---
+++ Day4/practice/ques2.py
@@ -0,0 +1,17 @@
+
+marks = []
+a = int(input("Enter marks here: "))
+marks.append(a)
+b = int(input("Enter marks here: "))
+marks.append(b)
+c = int(input("Enter marks here: "))
+marks.append(c)
+d = int(input("Enter marks here: "))
+marks.append(d)
+e = int(input("Enter marks here: "))
+marks.append(e)
+f = int(input("Enter marks here: "))
+marks.append(f)
+
+marks.sort()
+print(marks)
