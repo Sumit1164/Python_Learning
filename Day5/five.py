50111d917e59e149e7486854d4c8d9c0b2652268
Sumit tripathi <sumittripathi8795@gmail.com>
2026-04-26 13:52:44 +0530

    Dictionary & Sets

---
+++ Day5/five.py
@@ -0,0 +1,19 @@
+# Dictionary & Sets
+
+'''
+Dictionary is a collection of key value pair.
+it is unordered.
+it is mutable.
+is is indexed.
+cannot contain duplicate keys.
+'''
+
+marks ={
+    "Sumit" : 99.7,
+    "Shubh" : 89,
+    "Rax" : 78,
+    "Sax" : 92
+}
+
+# print(marks, type(marks))
+print(marks["Sumit"])
