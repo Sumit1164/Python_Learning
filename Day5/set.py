af9972540bd399866480bbd68eb5a96bc03dc1bf
Sumit tripathi <sumittripathi8795@gmail.com>
2026-05-02 17:14:43 +0530

    explore some advance method in set

--- Day5/set.py
+++ Day5/set.py
@@ -11,3 +11,23 @@ x = {1, 3, 5, 7, 9, "Zero", "MaxxUs7"}
 print(type(x))
 print(x)
 print(s)
+
+
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+
+a.union(b)         # {1, 2, 3, 4, 5}                a | b   # union
+a.intersection(b)  # {3}                            a & b   # intersection
+a.difference(b)    # {1, 2}                         a - b   # difference
+a.symmetric_difference(b)  # {1, 2, 4, 5}           a ^ b   # symmetric difference
+
+a.issubset(b)
+a.issuperset(b)
+a.isdisjoint(b)
+
+
+new_set = a.copy()
+
+
+fs = frozenset([1, 2, 3])   # cannot add or remove elements
