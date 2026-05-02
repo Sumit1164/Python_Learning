6e8a42bd6fd991d8083a37b33c8b0d099710a062
Sumit tripathi <sumittripathi8795@gmail.com>
2026-05-02 17:03:47 +0530

    set important methods

--- Day5/setMethods.py
+++ Day5/setMethods.py
@@ -1,2 +1,11 @@
 x = {1, 3, 5, 7, 9, "Zero", "MaxxUs7"}
 
+print(x, type(x))
+
+x.add(55) #Adding Elements # Adds a single element
+x.update([5, 6])  # Adds multiple elements
+x.remove(2)   # Removes element; error if not found
+x.discard(2)  # Removes element; no error if not found
+
+x.pop() # Removes a random element
+x.clear()     # Removes all elements
