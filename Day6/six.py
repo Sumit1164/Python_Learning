256e55e4daf1fa362c9c03f88cf7e3842dca9941
Sumit tripathi <sumittripathi8795@gmail.com>
2026-05-03 14:55:22 +0530

    Conditional expression in python

---
+++ Day6/six.py
@@ -0,0 +1,14 @@
+# Conditional expression
+
+a = int(input("Enter your age: "))
+
+if(a >= 18):
+    print("You are above the age of consent")
+
+elif(a < 0):
+    print("You are entering an invalid age")
+
+else:
+    print("You are below the age of consent")
+    
+print("End of the program")
