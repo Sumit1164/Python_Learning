cc7b290a550882a96ffbf361f4bb68e4cbd35a7e
Sumit tripathi <sumittripathi8795@gmail.com>
2026-05-15 18:30:15 +0530

    list, tuple, sets, string itreate using loop  in python

---
+++ Day7/ListDataF.py
@@ -0,0 +1,14 @@
+# Using For Loop to print List
+l = [1, 5, 6, 3, 5, 7, 4, 7, "Sumit", 4, 0, 9]
+for i in l:
+    print(i)
+
+# Using For Loop to print Tuple
+t = (6, 4, 8, 43, 67, 68, 404, 201)
+for i in t:
+    print(i)
+
+# Using For Loop to print String
+s = "SUMIT IS A GOOD MEN"
+for i in s:
+    print(i)
---
+++ Day7/listData.py
@@ -0,0 +1,8 @@
+l = [1, 5, "Sumit", True, "Shubh", 4, "Amit", 7]
+
+
+
+i = 0
+while(i <  len(l)):
+    print(l[i])
+    i+=1
