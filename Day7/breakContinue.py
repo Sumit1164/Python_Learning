f076223ca88533982b68b7e0b2ae744dbae04bdf
Sumit tripathi <sumittripathi8795@gmail.com>
2026-05-15 18:27:23 +0530

    break and continue in python

---
+++ Day7/breakContinue.py
@@ -0,0 +1,12 @@
+for i in range(20):
+    if(i == 7):
+        break   # EXIT THE LOOP RIGHT NOW
+    print(i)
+
+
+
+
+for i in range (20):
+    if(i == 7):
+        continue   # SKIP THIS ITERATION
+    print(i)
