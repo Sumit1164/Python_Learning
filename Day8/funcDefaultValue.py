98f0cedf85b80452ab78aa7379dacf3a6cef083e
Sumit tripathi <sumittripathi8795@gmail.com>
2026-05-17 13:59:09 +0530

    Default Parameter Value

---
+++ Day8/funcDefaultValue.py
@@ -0,0 +1,7 @@
+# Default Parameter Value
+
+def user(name, address="Noida"):
+    print("Good Morning, " + name + address)
+    # print(address)
+user("Sumit", " Tripathi, From Gorakhpur")
+user("Kashish ")
