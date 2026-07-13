457cd409a2cfbb0f6e8790f5b5f9f93fa5c92ffb
Sumit tripathi <sumittripathi8795@gmail.com>
2026-07-13 15:59:17 +0530

    singleInheritance

---
+++ Day12/singleInheritance.py
@@ -0,0 +1,15 @@
+class Employee:
+    company = "S7Frage InfoTech"
+    def show(self):
+        print(f"The name is {self.name} and the salary is {self.salary}")
+
+class Programmer(Employee):
+    company = "S7Frage"
+    def showLanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language")
+
+
+a = Employee()
+b = Programmer()
+
+print(f"The company of a is: {a.company}\nThe company of b is: {b.company}")
